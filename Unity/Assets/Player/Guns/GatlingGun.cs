a50c1518f21fb4ef629355dcfe0d9660a1cad91b
Liz Izatt <Liz@DESKTOP-73EUVH8.(none)>
2017-12-03 05:16:01 -0800

    Gitignore, gun sounds

--- Unity/Assets/Player/Guns/GatlingGun.cs
+++ Unity/Assets/Player/Guns/GatlingGun.cs
@@ -4,13 +4,28 @@ using System.Collections;
 public class GatlingGun : GunBase {
 
 	public GameObject bulletPrefab = null;
-		
+	public AudioClip bulletSound = null;
+	public AudioClip bulletDropSound = null;
+	public AudioSource audioSource = null;
+
+	public void Start()
+	{
+		audioSource = GetComponent<AudioSource> ();
+	}
+
 	public override void Fire()
 	{
 		GameObject newBullet = GameObject.Instantiate (bulletPrefab);
 		newBullet.transform.right = transform.right;
 		newBullet.transform.position = transform.position + transform.rotation * new Vector3 (3.5f, 2.75f, 0.0f);
 		newBullet.GetComponent<BulletController>().fire();
+
+		if (bulletSound != null) {
+			audioSource.PlayOneShot (bulletSound);
+		}
+		if (bulletDropSound != null) {
+			audioSource.PlayOneShot (bulletDropSound);
+		}
 	}
 
 	public override bool Auto()
--- Unity/Assets/Player/Guns/Pistol.cs
+++ Unity/Assets/Player/Guns/Pistol.cs
@@ -4,12 +4,24 @@ using System.Collections;
 public class Pistol : GunBase {
 
 	public GameObject bulletPrefab = null;
+	public AudioClip bulletSound = null;
+	AudioSource audioSource = null;
+
+	public void Start()
+	{
+		audioSource = GetComponent<AudioSource> ();
+	}
+
 	public override void Fire()
 	{
 		GameObject newBullet = GameObject.Instantiate (bulletPrefab);
 		newBullet.transform.right = transform.right;
 		newBullet.transform.position = transform.position + transform.rotation * new Vector3 (3.5f, 2.75f, 0.0f);
 		newBullet.GetComponent<BulletController>().fire();
+
+		if (bulletSound != null) {
+			audioSource.PlayOneShot (bulletSound);
+		}
 	}
 
 	public override bool Auto()
